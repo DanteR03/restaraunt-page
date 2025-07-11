b40ab178d108f0da5d1b2c3dd2c8163e22e21ae1
Lev <101640837+DanteR03@users.noreply.github.com.>
2025-07-12 03:09:10 +0500

    Create menu page structure in JS on button click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { mainPageLoad } from "./main_page/main_page.js";
+import { menuPageLoad } from "./menu_page/menu_page.js";
 import { contactPageLoad } from "./contact_page/contact_page.js";
 
 mainPageLoad()
@@ -8,4 +9,5 @@ let menuButton = document.querySelector("#menu");
 let contactButton = document.querySelector("#contact");
 
 homeButton.addEventListener("click", mainPageLoad);
+menuButton.addEventListener("click", menuPageLoad);
 contactButton.addEventListener("click", contactPageLoad);
---
+++ src/menu_page/menu_page.js
@@ -0,0 +1,48 @@
+import menuImageOne from "./menu_img1.jpg";
+import menuImageTwo from "./menu_img2.jpg";
+import menuImageThree from "./menu_img3.jpg";
+import menuImageFour from "./menu_img4.jpg";
+
+function menuPageLoad () {
+    let mainContentContainer = document.querySelector("#content");
+    mainContentContainer.innerHTML = "";
+    let menuItemContainerOne = document.createElement("div");
+    let menuItemContainerTwo = document.createElement("div");
+    let menuItemContainerThree = document.createElement("div");
+    let menuItemContainerFour = document.createElement("div");
+    let menuItemImageOne = document.createElement("img");
+    let menuItemNameOne = document.createElement("para");
+    let menuItemPriceOne = document.createElement("para");
+    menuItemImageOne.src = menuImageOne;
+    menuItemImageOne.alt = "A picture of pizza";
+    menuItemNameOne.textContent = "Pepperoni Pizza";
+    menuItemPriceOne.textContent = "150";
+    let menuItemImageTwo = document.createElement("img");
+    let menuItemNameTwo = document.createElement("para");
+    let menuItemPriceTwo = document.createElement("para");
+    menuItemImageTwo.src = menuImageTwo;
+    menuItemImageTwo.alt = "A picture of pizza";
+    menuItemNameTwo.textContent = "Spicy Chicken Pizza";
+    menuItemPriceTwo.textContent = "170";
+    let menuItemImageThree = document.createElement("img");
+    let menuItemNameThree = document.createElement("para");
+    let menuItemPriceThree = document.createElement("para");
+    menuItemImageThree.src = menuImageThree;
+    menuItemImageThree.alt = "A picture of pizza";
+    menuItemNameThree.textContent = "Cheesy Pizza";
+    menuItemPriceThree.textContent = "200";
+    let menuItemImageFour = document.createElement("img");
+    let menuItemNameFour = document.createElement("para");
+    let menuItemPriceFour = document.createElement("para");
+    menuItemImageFour.src = menuImageFour;
+    menuItemImageFour.alt = "A picture of pizza";
+    menuItemNameFour.textContent = "Mexican Pizza";
+    menuItemPriceFour.textContent = "165";
+    menuItemContainerOne.append(menuItemImageOne, menuItemNameOne, menuItemPriceOne);
+    menuItemContainerTwo.append(menuItemImageTwo, menuItemNameTwo, menuItemPriceTwo);
+    menuItemContainerThree.append(menuItemImageThree, menuItemNameThree, menuItemPriceThree);
+    menuItemContainerFour.append(menuItemImageFour, menuItemNameFour, menuItemPriceFour);
+    mainContentContainer.append(menuItemContainerOne, menuItemContainerTwo, menuItemContainerThree, menuItemContainerFour);
+}
+
+export { menuPageLoad }
